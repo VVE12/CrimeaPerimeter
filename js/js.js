a38f39fab14ee95979d80999b337ae079a297a2f
Vladislav <vasilievvladislav12@gmail.com>
2017-08-03 09:27:40 +0300

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,10 @@
+  $('.sl').slick({
+  	autoplay: true,
+  	autoplaySpeed: 3000,
+  	speed: 3000,
+  	cssEase: 'linear',
+  	centerMode: true,
+    pauseOnHover: true,
+    slideToShow: 6,
+    slideToScroll: 6,
+  });
